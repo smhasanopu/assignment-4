551111411fb8b0948ca463e58583ed5be8562359
Syed Mahadi Hasan Opu <smhasanopu@gmail.com>
2021-08-09 11:18:37 +0600

    2nd Commit

--- assignment.js
+++ assignment.js
@@ -6,6 +6,31 @@
 
 // Problem No -1
 
+/* function  seerToMon(seer) {
+    const moon = seer / 40;
+    return moon;  
+}
+
+const totalMoon = seerToMon(120);
+console.log("Total", totalMoon "Moon "); */
+
+// // Problem No -2
+
+function  totalSales(sartQuantity ,pantQuantity,kadesQuantity) {
+
+    const perSartPrice  = 100;
+    const perPantPrice  = 200;
+    const perkadesPrice = 500;
+
+    const sartQ  = sartQuantity  * perSartPrice;
+    const pantQ  = pantQuantity  * perPantPrice;
+    const cadesQ = kadesQuantity * perkadesPrice;
 
 
+    const totalproduct = sartQ +pantQ + cadesQ;
+    return totalproduct;
+}
+
+const total = totalSales(0,0,5);
+console.log(total);
 
